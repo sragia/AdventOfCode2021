9af5ae3c99623ac78ae692314d915fedd4f51d82
Aigars Bedeicis <a.bedeicis@gmail.com>
2021-12-02 21:21:55 +0200

    day 2

---
+++ day02/index.ts
@@ -0,0 +1,70 @@
+import { readFile } from "fs";
+import { inputToArray } from "../helper";
+
+const parseInputDirections = (input: {direction: 'forward' | 'down' | 'up'; value: number}[]): {horizontal: number; depth: number} => {
+    let horizontal = 0;
+    let depth = 0;
+
+    input.forEach((inp) => {
+        switch(inp.direction) {
+            case 'down':
+                depth += inp.value;
+                break;
+            case 'forward':
+                horizontal += inp.value;
+                break;
+            case 'up':
+                depth -= inp.value;
+        }
+    })
+
+    return {
+        horizontal,
+        depth
+    };
+}
+
+const parseInputDirectionsWithAim = (input: {direction: 'forward' | 'down' | 'up'; value: number}[]): {horizontal: number; depth: number} => {
+    let horizontal = 0;
+    let depth = 0;
+    let aim = 0
+
+    input.forEach((inp) => {
+        switch(inp.direction) {
+            case 'down':
+                aim += inp.value;
+                break;
+            case 'forward':
+                horizontal += inp.value;
+                depth += inp.value * aim;
+                break;
+            case 'up':
+                aim -= inp.value;
+        }
+    })
+
+    return {
+        horizontal,
+        depth
+    };
+}
+
+
+inputToArray(__dirname + '/input.txt', (data: any[]) => {
+    const input = data.map(item => {
+        if (item) {
+            const parts = item.split(' ');
+            return {
+                direction: parts[0],
+                value: parseInt(parts[1], 10)
+            }
+        }
+    }).filter(Boolean);
+
+    const result = parseInputDirections(input);
+    console.log('Result 1 -' , result.depth * result.horizontal, result);
+
+    
+    const result2 = parseInputDirectionsWithAim(input);
+    console.log('Result 2 -' , result2.depth * result2.horizontal, result2);
+});
